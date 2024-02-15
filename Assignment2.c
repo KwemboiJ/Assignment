2346245c0d34c2aacd4f040de486258e56ef032e
KwemboiJ <joshuakwemboi03@gmail.com>
2024-02-15 16:55:52 +0300

    2dp

--- Assignment2.c
+++ Assignment2.c
@@ -9,19 +9,19 @@ for (int i=0; i<4; i++){
 
 if (i==0)
 {
-printf("The Sum of %1f and %1f is %1f.\n",a,b,a+b);
+printf("The Sum of %.2f and %.2f is %.2f.\n",a,b,a+b);
 }
 else if (i==1)
 {
-printf("The Difference of %f and %f is %f.\n",a,b,a-b);
+printf("The Difference of %.2f and %.2f is %f.\n",a,b,a-b);
 }
 else if (i==2 )
 {
-printf("The Multiplication of %f and %f is %f.\n",a,b,a*b);
+printf("The Multiplication of %.2f and %.2f is %.2f.\n",a,b,a*b);
 }
 else if (i==3)
 {
-printf("The Division of %f and %f is %f.\n",a,b,a/b);
+printf("The Division of %.2f and %.2f is %.2f.\n",a,b,a/b);
 } else 
 break;
 }
