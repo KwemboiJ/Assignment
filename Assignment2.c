8e0145406ddbfc22786e3fbed1ab56acfe49d980
KwemboiJ <joshuakwemboi03@gmail.com>
2024-02-18 11:02:14 +0300

    with various functions

--- Assignment2.c
+++ Assignment2.c
@@ -1,28 +1,27 @@
+//Joshua Kwemboi ENE212-0088/2021
 #include<stdio.h>
+float Sum(float,float),Difference(float,float),Division(float,float),Multiplication(float,float);//Function Declaration
 int main ()
 {
-float a,b;
+float a,b,Mul,Div,Sum1,Diff;
 printf("Enter a and b: ");
 scanf("%f  %f",&a,&b);
+//Calling
+Sum1= Sum(a,b);
+Diff= Difference(a, b);
+Mul=Multiplication(a, b);
+Div=Division(a, b);
+//output
+printf("The sum of %.2f and %.2f is %.2f.\n",a,b,Sum1);
+printf("The Difference of %.2f and %.2f is %.2f.\n",a,b,Diff);
+printf("The Division of %.2f and %.2f is %.2f.\n",a,b,Div);
+printf("The Multiplication of %.2f and %.2f is %.2f.\n",a,b,Mul);
+return 0;
+}
+// Fuction definition
+float Sum(float a,float b){return a+b;}
+float Difference(float a,float b){return a-b;}
+float Multiplication(float a,float b){return a*b;}
+float Division(float a,float b){return a/b;}
 
-for (int i=0; i<4; i++){
 
-if (i==0)
-{
-printf("The Sum of %.2f and %.2f is %.2f.\n",a,b,a+b);
-}
-else if (i==1)
-{
-printf("The Difference of %.2f and %.2f is %f.\n",a,b,a-b);
-}
-else if (i==2 )
-{
-printf("The Multiplication of %.2f and %.2f is %.2f.\n",a,b,a*b);
-}
-else if (i==3)
-{
-printf("The Division of %.2f and %.2f is %.2f.\n",a,b,a/b);
-} else 
-break;
-}
-}
